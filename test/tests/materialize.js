737a2c1147917fb8295410c45112a7c1d7ff3e07
Aleksei <aleksey.mrlss@gmail.com>
2020-08-20 20:16:12 +0300

    workaround for git actions error

--- test/tests/materialize.js
+++ test/tests/materialize.js
@@ -20,89 +20,118 @@ function materializeTest({staticExpectedFiles = [], templatesFilesPath, expected
     const testSettings = {...prompts, ...generalSettings, expectedFilesContent, staticExpectedFiles};
 
     describe(projectTypeMessage(testSettings), async () => {
-      before(async (done) => {
+      before(async () => {
         await cleanUpFolder();
-        done();
         return helpers.run(PATHS.appFolder).cd(PATHS.tempFolder).withPrompts(testSettings);
       });
 
       describe('Generating files:', () => {
-        it(chalk.green('Create expected files'), async (done) => {
-          setProcessToDestination();
+        it(chalk.green('Create expected files'), () => {
+          return new Promise(async (resolve, reject) => {
+            try {
+              await setProcessToDestination();
 
-          const unexpectedFiles = testSettings.staticUnexpectedFiles;
-          const expectedFiles = [...(await getFilesArray(templatesFilesPath))]
-            .concat([...(await getFilesArray(join(PATHS.templatesFolder, PROMPTS_VALUES.framework.materialize)))])
-            .concat(testSettings.staticExpectedFiles);
+              const unexpectedFiles = testSettings.staticUnexpectedFiles;
+              const expectedFiles = [...(await getFilesArray(templatesFilesPath))]
+                .concat([...(await getFilesArray(join(PATHS.templatesFolder, PROMPTS_VALUES.framework.materialize)))])
+                .concat(testSettings.staticExpectedFiles);
 
-          yeomanAssert.file(expectedFiles);
-          yeomanAssert.noFile(unexpectedFiles);
-          done();
+              yeomanAssert.file(expectedFiles);
+              yeomanAssert.noFile(unexpectedFiles);
+              resolve();
+            } catch (err) {
+              reject(err);
+            }
+          });
         });
       });
 
       describe('Checking dependencies:', () => {
-        it(chalk.green('Setup settings'), (done) => {
-          setProcessToDestination();
+        it(chalk.green('Setup settings'), () => {
+          return new Promise(async (resolve, reject) => {
+            try {
+              await setProcessToDestination();
 
-          const newCfg = JSON.parse(fs.readFileSync(join(PATHS.tempMarkupFolder, 'config.json')));
-          const newPkgfilePath = join(PATHS.tempMarkupFolder, 'package.json');
-          const jsFile = join(PATHS.tempMarkupFolder, newCfg.src, newCfg.scripts.src, `${newCfg.scripts.bundle}.${newCfg.scripts.extension}`);
-          const stylesFile = join(PATHS.tempMarkupFolder, newCfg.src, newCfg.styles.src, `${newCfg.styles.bundle}.${newCfg.styles.extension}`);
+              const newCfg = JSON.parse(fs.readFileSync(join(PATHS.tempMarkupFolder, 'config.json')));
+              const newPkgfilePath = join(PATHS.tempMarkupFolder, 'package.json');
+              const jsFile = join(PATHS.tempMarkupFolder, newCfg.src, newCfg.scripts.src, `${newCfg.scripts.bundle}.${newCfg.scripts.extension}`);
+              const stylesFile = join(PATHS.tempMarkupFolder, newCfg.src, newCfg.styles.src, `${newCfg.styles.bundle}.${newCfg.styles.extension}`);
 
-          it(chalk.green('Library imported into JS:'), () => {
-            testSettings.expectedFilesContent.js.map((content) => yeomanAssert.fileContent(jsFile, content));
+              it(chalk.green('Library imported into JS:'), () => {
+                testSettings.expectedFilesContent.js.map((content) => yeomanAssert.fileContent(jsFile, content));
+              });
+
+              it(chalk.green('Library imported into Styles:'), () => {
+                testSettings.expectedFilesContent.styles.map((content) => yeomanAssert.fileContent(stylesFile, content));
+              });
+
+              it(chalk.green('Modules added to package.json:'), () => {
+                testSettings.expectedFilesContent.json.map((content) => yeomanAssert.jsonFileContent(newPkgfilePath, content));
+              });
+
+              resolve();
+            } catch (error) {
+              reject(err);
+            }
           });
-
-          it(chalk.green('Library imported into Styles:'), () => {
-            testSettings.expectedFilesContent.styles.map((content) => yeomanAssert.fileContent(stylesFile, content));
-          });
-
-          it(chalk.green('Modules added to package.json:'), () => {
-            testSettings.expectedFilesContent.json.map((content) => yeomanAssert.jsonFileContent(newPkgfilePath, content));
-          });
-
-          done();
         });
       });
 
       if (!ONLY_FILES_TEST) {
         describe('Installing dependencies:', () => {
-          it(chalk.green('Install all dependencies'), async (done) => {
-            const cli = await chaiExecAsync(SCRIPTS.install);
-            assert.exitCode(cli, 0);
-            done();
+          it(chalk.green('Install all dependencies'), () => {
+            return new Promise(async (resolve, reject) => {
+              try {
+                const cli = await chaiExecAsync(SCRIPTS.install);
+                assert.exitCode(cli, 0);
+                resolve();
+              } catch (error) {
+                reject(err);
+              }
+            });
           });
         });
 
         describe('Running build process:', () => {
-          it(chalk.green('Build process is correct:'), async (done) => {
-            const cli = await chaiExecAsync(SCRIPTS.build);
-            assert.exitCode(cli, 0);
-            done();
+          it(chalk.green('Build process is correct:'), () => {
+            return new Promise(async (resolve, reject) => {
+              try {
+                const cli = await chaiExecAsync(SCRIPTS.build);
+                assert.exitCode(cli, 0);
+                resolve();
+              } catch (error) {
+                reject(err);
+              }
+            });
           });
         });
 
         describe('Building correct files:', () => {
-          it(chalk.green('Generate all files based on project config'), async (done) => {
-            setProcessToDestination();
-            const newCfg = JSON.parse(fs.readFileSync(join(PATHS.tempMarkupFolder, 'config.json')));
-            const getStyleFile = () => {
-              return join(newCfg.dest, newCfg.styles.dest, `${newCfg.styles.bundle}.css`);
-            };
+          it(chalk.green('Generate all files based on project config'), () => {
+            return new Promise(async (resolve, reject) => {
+              try {
+                await setProcessToDestination();
+                const newCfg = JSON.parse(fs.readFileSync(join(PATHS.tempMarkupFolder, 'config.json')));
+                const getStyleFile = () => {
+                  return join(newCfg.dest, newCfg.styles.dest, `${newCfg.styles.bundle}.css`);
+                };
 
-            const stylesFile = getStyleFile();
-            const jsFile = join(newCfg.dest, newCfg.scripts.dest, `${newCfg.scripts.bundle}.${newCfg.scripts.extension}`);
-            const HTMLFiles = join(newCfg.dest, newCfg.templates.dest, `*.${newCfg.templates.extension}`);
+                const stylesFile = getStyleFile();
+                const jsFile = join(newCfg.dest, newCfg.scripts.dest, `${newCfg.scripts.bundle}.${newCfg.scripts.extension}`);
+                const HTMLFiles = join(newCfg.dest, newCfg.templates.dest, `*.${newCfg.templates.extension}`);
 
-            const expectedCompilation = [stylesFile, jsFile];
+                const expectedCompilation = [stylesFile, jsFile];
 
-            glob(HTMLFiles, {}, (err, files) => {
-              expectedCompilation.push(...files);
-              yeomanAssert.file(expectedCompilation);
+                glob(HTMLFiles, {}, (err, files) => {
+                  expectedCompilation.push(...files);
+                  yeomanAssert.file(expectedCompilation);
+                });
+
+                resolve();
+              } catch (error) {
+                reject(err);
+              }
             });
-
-            done();
           });
         });
       }
