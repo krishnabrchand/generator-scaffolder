e3a1c5a18f98f042d060c728b7390096cb077c76
Aleksei <aleksey.mrlss@gmail.com>
2020-08-20 18:31:14 +0300

    test with promise

--- test/index.js
+++ test/index.js
@@ -8,39 +8,56 @@ const {TESTS_SETTINGS} = require(`${process.cwd()}/generators/app/globals`);
 const {mergeDeep} = require(`${process.cwd()}/generators/app/utils`);
 
 const title = (text) => {
-  return chalk.bgCyan(chalk.bold.black(`
+  return chalk.bgCyan(
+    chalk.bold.black(`
     ****************************
     ${text}
-    ****************************`))
-}
+    ****************************`)
+  );
+};
 
 const getProjectSettings = (target, source) => {
   const settings = mergeDeep(Object.assign({}, target), source);
 
   return settings;
-}
+};
 
 for (const key in TESTS_SETTINGS.templating) {
   if (TESTS_SETTINGS.templating.hasOwnProperty(key)) {
     const templatesFiles = TESTS_SETTINGS.templating[key];
 
     describe(title(`Markup + ${key}`), () => {
-      defaultTest(getProjectSettings(TESTS_SETTINGS.markup.default, templatesFiles));
-      bootstrapTest(getProjectSettings(TESTS_SETTINGS.markup.bootstrap, templatesFiles));
-      materializeTest(getProjectSettings(TESTS_SETTINGS.markup.materialize, templatesFiles));
-      tailwindTest(getProjectSettings(TESTS_SETTINGS.markup.tailwind, templatesFiles));
-      zurbTest(getProjectSettings(TESTS_SETTINGS.markup.zurb, templatesFiles));
+      return new Promise(async (resolve, reject) => {
+        try {
+          await Promise.all([
+            defaultTest(getProjectSettings(TESTS_SETTINGS.markup.default, templatesFiles)),
+            bootstrapTest(getProjectSettings(TESTS_SETTINGS.markup.bootstrap, templatesFiles)),
+            materializeTest(getProjectSettings(TESTS_SETTINGS.markup.materialize, templatesFiles)),
+            tailwindTest(getProjectSettings(TESTS_SETTINGS.markup.tailwind, templatesFiles)),
+            zurbTest(getProjectSettings(TESTS_SETTINGS.markup.zurb, templatesFiles)),
+          ]);
+          resolve();
+        } catch (error) {
+          reject(error);
+        }
+      });
     });
 
     describe(title(`Markup + Wordpress + ${key}`), () => {
-      defaultTest(getProjectSettings(TESTS_SETTINGS.wp.default, templatesFiles));
-      bootstrapTest(getProjectSettings(TESTS_SETTINGS.wp.bootstrap, templatesFiles));
-      materializeTest(getProjectSettings(TESTS_SETTINGS.wp.materialize, templatesFiles));
-      tailwindTest(getProjectSettings(TESTS_SETTINGS.wp.tailwind, templatesFiles));
-      zurbTest(getProjectSettings(TESTS_SETTINGS.wp.zurb, templatesFiles));
+      return new Promise(async (resolve, reject) => {
+        try {
+          await Promise.all([
+            defaultTest(getProjectSettings(TESTS_SETTINGS.wp.default, templatesFiles)),
+            bootstrapTest(getProjectSettings(TESTS_SETTINGS.wp.bootstrap, templatesFiles)),
+            materializeTest(getProjectSettings(TESTS_SETTINGS.wp.materialize, templatesFiles)),
+            tailwindTest(getProjectSettings(TESTS_SETTINGS.wp.tailwind, templatesFiles)),
+            zurbTest(getProjectSettings(TESTS_SETTINGS.wp.zurb, templatesFiles)),
+          ]);
+          resolve();
+        } catch (error) {
+          reject(error);
+        }
+      });
     });
   }
 }
-
-
-
